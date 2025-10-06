d6dab28b4aacd54dce8d080955a2d9685f9d0e3c
Amr-Shams <amr.shams2015.as@gmail.com>
2025-10-06 07:04:37 +0300

    add: new tool ff ffind files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -33,7 +33,6 @@ return {
 
     keymap("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
     keymap("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
-    keymap("n", "<leader>sf", builtin.find_files, { desc = "Search Files" })
     keymap("n", "<leader>ss", builtin.builtin, { desc = "Search Select Telescope" })
     keymap("n", "<leader>sw", builtin.grep_string, { desc = "Search current Word" })
     keymap("n", "<leader>sg", builtin.live_grep, { desc = "Search by Grep" })
